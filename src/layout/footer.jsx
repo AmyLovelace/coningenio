caa80c2dc316563f7598dfb20307ed723ad98474
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2025-04-20 20:18:23 -0400

    se arregla scroll al navegar a contacto y se renderiza condicionalmente el ver mas del footer

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Footer = ({lang}) => {
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const translations = {
     MORE: {
@@ -10,19 +13,23 @@ const Footer = ({lang}) => {
    
   };
 
-
+ const handleNavigate = () => {
+    navigate('/contacto');
+    window.scrollTo(0, 0); 
+  };
 
   return (
 
       <section className="container-fluid footer_section">
+          {location.pathname !== '/contacto' && ( 
           <div className="container mt-5">
               <div className="btn-box">
-                  <a href="/contacto">
+                  <a onClick={handleNavigate}>
                   {translations.MORE[lang]}
                   </a>
                   <hr />
               </div>
-          </div>
+          </div>)}
           <p>
               Copyright &copy; 2025 Desarrollo Backend M-50
               <a>IPSS</a>
